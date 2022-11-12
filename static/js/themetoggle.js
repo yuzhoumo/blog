ff7e4ef049c70cc020e2a32600dc08bc9c2eaac6
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-11-12 15:30:09 -0800

    Add blinking cursor animation and refactor (#9)
    * Add blinking cursor animation and refactor CSS
    
    * Add navbar title override option
    
    * Add config toggle for blinking cursor animation
    
    * Clean up HTML formatting

--- static/js/themetoggle.js
+++ static/js/themetoggle.js
@@ -1,13 +1,16 @@
 function setTheme(mode) {
   localStorage.setItem("theme-storage", mode);
+
   if (mode === "dark") {
-    document.getElementById("darkModeStyle").disabled=false;
-    document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
-    feather.replace()
+    document.getElementById("darkModeStyle").disabled = false;
+    document.getElementById("dark-mode-toggle").innerHTML =
+      "<i data-feather=\"sun\"></i>";
+    feather.replace({ id: "dark-mode-toggle-icon" });
   } else if (mode === "light") {
-    document.getElementById("darkModeStyle").disabled=true;
-    document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
-    feather.replace()
+    document.getElementById("darkModeStyle").disabled = true;
+    document.getElementById("dark-mode-toggle").innerHTML =
+      "<i data-feather=\"moon\"></i>";
+    feather.replace({ id: "dark-mode-toggle-icon" });
   }
 }
 
